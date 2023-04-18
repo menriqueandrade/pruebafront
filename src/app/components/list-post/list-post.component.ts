8ff1dc36c92411715360c29aaad67b025391f13f
menriqueandrade <56304628+menriqueandrade@users.noreply.github.com>
2023-04-18 18:33:13 -0500

    list: done

--- src/app/components/list-post/list-post.component.ts
+++ src/app/components/list-post/list-post.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostApiService } from 'src/app/services/post-api.service';
 
 @Component({
   selector: 'app-list-post',
   templateUrl: './list-post.component.html',
   styleUrls: ['./list-post.component.css']
 })
-export class ListPostComponent {
+export class ListPostComponent implements OnInit {
+  post:any;
+  constructor(
+    private postService: PostApiService,
+
+
+  ) { }
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.postService.getPosts().subscribe(response => {
+        console.log(response);
+        this.post = response;
+      });
+    },
+      500);
+  }
+
+  deleteEmployee(id: any, iControl: any) {
+    console.log(id);
+    console.log(iControl);
+    if (window.confirm("¿Esta seguro de borrar??")) {
+      this.postService.deleteEmployee(id).subscribe((response) => {
+        console.log(response);
+        //indice de control frame
+        this.post.splice(iControl, 1)
+      });
+    }
+  }
 
 }
--- src/app/services/post-api.service.ts
+++ src/app/services/post-api.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostApiService {
+  API: string = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor() { }
+  constructor(private postHttp: HttpClient) { 
+    
+  }
+  getPosts(){
+    return this.postHttp.get(this.API)
+  }
+  deleteEmployee(id: any): Observable<any> {
+
+    return this.postHttp.get(this.API + "/" + id)
+  }
 }
